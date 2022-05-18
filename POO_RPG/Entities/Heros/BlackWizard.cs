d268b0b055b2dbc02bac11cd0e642e9703a283cc
JacksonSM <Jacksons.m2018@gmail.com>
2022-05-18 20:21:54 -0300

    Implementado a classe BlackWizard

---
+++ POO_RPG/Entities/Heros/BlackWizard.cs
@@ -0,0 +1,24 @@
+﻿using POO_RPG.Entities.Enums;
+
+namespace POO_RPG.Entities.Heros
+{
+    public class BlackWizard : Hero
+    {
+        public WizardMagic Magic { get; set; }
+
+        public BlackWizard(string name, int level, int hP, int mP)
+        {
+            Name = name;
+            Level = level;
+            HP = hP;
+            MP = mP;
+            HeroType = HeroType.Wizard;
+
+        }
+
+        public override string Attack()
+        {
+            return $"{this.Name} causou um dano de {((int)this.Magic)} com sua {this.Magic}";
+        }
+    }
+}
