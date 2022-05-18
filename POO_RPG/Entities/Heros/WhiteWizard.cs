2b2f81f39e8686eaeca417ca29798d07d75b5fa4
JacksonSM <Jacksons.m2018@gmail.com>
2022-05-18 20:17:47 -0300

    Implementado a classe WhiteWizard

---
+++ POO_RPG/Entities/Heros/WhiteWizard.cs
@@ -0,0 +1,24 @@
+﻿using POO_RPG.Entities.Enums;
+
+namespace POO_RPG.Entities.Heros
+{
+    public class WhiteWizard : Hero
+    {
+        public WizardMagic Magic { get; set; }
+
+        public WhiteWizard(string name, int level, int hP, int mP)
+        {
+            Name = name;
+            Level = level;
+            HP = hP;
+            MP = mP;
+            HeroType = HeroType.WhiteWizard;
+
+        }
+
+        public override string Attack()
+        {
+            return $"{this.Name} causou um dano de {((int)this.Magic)} com sua {this.Magic}";
+        }
+    }
+}
