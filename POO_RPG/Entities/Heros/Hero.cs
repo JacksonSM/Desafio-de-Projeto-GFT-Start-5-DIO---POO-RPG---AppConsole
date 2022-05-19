2e9c031909a0ebc9076f45c364d3198e983c2cac
JacksonSM <Jacksons.m2018@gmail.com>
2022-05-18 21:10:04 -0300

    Alterado modificador de acesso HP e MP do Hero.

--- POO_RPG/Entities/Heros/Hero.cs
+++ POO_RPG/Entities/Heros/Hero.cs
@@ -6,8 +6,8 @@ namespace POO_RPG.Entities.Heros
     {
         public string Name { get; set; }
         public int Level { get; set; }
-        public int HP { get; set; }
-        public int MP { get; set; }
+        public int HP { get; protected set; }
+        public int MP { get; protected set; }
         public  HeroType HeroType { get; set; }
 
        
